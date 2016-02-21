a34648feb12cba3d87e5e9e7309e594239a48eb9
tazim92 <tazim.ndc@gmail.com>
2016-02-21 20:39:33 +0600

    #1 Update 2

---
+++ app/src/main/java/com/returnnull/rukhedarae/TortureType.java
@@ -0,0 +1,13 @@
+package com.returnnull.rukhedarae;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TortureType extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_torture_type);
+    }
+}
