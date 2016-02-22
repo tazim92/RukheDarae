802e93d5ecc4d0e2e6f97e6cfcf1dbd43996ea7c
tazim92 <tazim.ndc@gmail.com>
2016-02-22 08:51:04 +0600

    org numbers

---
+++ app/src/main/java/com/returnnull/rukhedarae/Emergency_org_numbers.java
@@ -0,0 +1,13 @@
+package com.returnnull.rukhedarae;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Emergency_org_numbers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergency_org_numbers);
+    }
+}
